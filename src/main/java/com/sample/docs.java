9d50d4488cf8482367c408146b716b03fec19625
Nikhil <Sknikhil2000@gmail.com>
2022-12-23 09:58:33 +0530

    docs for project

---
+++ src/main/java/com/sample/docs.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class docs {
+    public static void main(String[] args) {
+        /*
+        this is my backend project
+        using gradle as build tool
+        java as language
+        spring boot as framework
+         */
+    }
+}
